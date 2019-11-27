9d769da6e8267f5b7a64aa4d8bfe60d3e882194c
Giorgi Gabunia <ggabunia@tbcinsurance.ge>
2019-11-27 19:05:10 +0400

    added api project

---
+++ StudentRegistryAPI/Controllers/StudentsController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using StudentRegistry.DataAccess;
+using StudentRegistry.Models;
+
+namespace StudentRegistryAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        private IUnitOfWork _context;
+
+        public StudentsController(IUnitOfWork context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Students
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Student>>> GetStudents()
+        {
+            return await Task.FromResult(_context.StudentRepository.Get(includeProperties: "Gender").ToList());
+        }
+
+        // GET: api/Students/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Student>> GetStudent(int id)
+        {
+            var student = await Task.FromResult(_context.StudentRepository.GetByID(id));
+
+            if (student == null)
+            {
+                return NotFound();
+            }
+
+            return student;
+        }
+
+        // PUT: api/Students/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutStudent(int id, Student student)
+        {
+            if (id != student.Id)
+            {
+                return BadRequest();
+            }
+
+            try
+            {
+                _context.StudentRepository.Update(student);
+                _context.Save();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!StudentExists(student.Id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        //// POST: api/Students
+        //// To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        //// more details see https://aka.ms/RazorPagesCRUD.
+        //[HttpPost]
+        //public async Task<ActionResult<Student>> PostStudent(Student student)
+        //{
+        //    _context.Students.Add(student);
+        //    await _context.SaveChangesAsync();
+
+        //    return CreatedAtAction("GetStudent", new { id = student.Id }, student);
+        //}
+
+        //// DELETE: api/Students/5
+        //[HttpDelete("{id}")]
+        //public async Task<ActionResult<Student>> DeleteStudent(int id)
+        //{
+        //    var student = await _context.Students.FindAsync(id);
+        //    if (student == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    _context.Students.Remove(student);
+        //    await _context.SaveChangesAsync();
+
+        //    return student;
+        //}
+
+        private bool StudentExists(int id)
+        {
+            return _context.StudentRepository.GetByID(id) != null;
+        }
+    }
+}
